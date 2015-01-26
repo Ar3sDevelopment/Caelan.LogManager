1f9e54bdb83b47e09f615b685dc244465703b89f
Matteo Bruni <ar3shack@gmail.com>
2015-01-26 21:37:40 +0100

    Added configuration manager

---
+++ Caelan.LogManager/LogConfiguration.fs
@@ -0,0 +1,15 @@
+﻿namespace Caelan.LogManager
+
+open System
+open System.Configuration
+
+type FileWriterElement() =
+    inherit ConfigurationElement()
+
+    member val Path = "" with get, set
+
+type LogConfiguration() = 
+    inherit ConfigurationSection()
+
+    [<ConfigurationProperty("fileWriter", IsKey = true)>]
+    member val Test : FileWriterElement option = None with get, set
