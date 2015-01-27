3383e42c85664fac2cdb42b4bc55897d24ac045d
Matteo Bruni <ar3shack@gmail.com>
2015-01-27 08:35:07 +0100

    Missing file

---
+++ Caelan.LogManager/FileLogWriter.fs
@@ -0,0 +1,16 @@
+﻿namespace Caelan.LogManager
+
+open System
+open System.IO
+
+type FileLogWriter(path) = 
+    interface ILogWriter with
+        member __.Name with get() = "FileLogWriter"
+
+        member __.Log logType message =
+            File.AppendAllText(path, message + Environment.NewLine)
+        
+        member __.LogException logType message ex =
+            File.AppendAllText(path, "msg: " + message + ", ex: " + ex.Message + Environment.NewLine)
+
+    member val Path = path with get, set
